e276f45951d0a79a9a8613ec9fb1a2c3dde12977
hzquguoqing <hzquguoqing@corp.netease.com>
2015-09-17 18:51:20 +0800

    增加storm参数，到配置文件

--- watchlog-storm/src/main/java/com/netease/qa/log/storm/spouts/NginxReader.java
+++ watchlog-storm/src/main/java/com/netease/qa/log/storm/spouts/NginxReader.java
@@ -2,11 +2,22 @@ package com.netease.qa.log.storm.spouts;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+
+
+
+
+import com.netease.qa.log.storm.util.ConfigReader;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
 
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
@@ -22,7 +33,11 @@ public class NginxReader implements IRichSpout {
 	private SpoutOutputCollector collector;
 	
 	private static final Logger logger = LoggerFactory.getLogger(NginxReader.class);
-	private static int count = 0;
+	private static Channel channel;
+	private static Connection connection;
+	private static String queueName = ConfigReader.MQ_QUEUE;
+	private static String host = ConfigReader.MQ_HOST;
+	private static int port = ConfigReader.MQ_PORT;
 
 	/*
 	 * (non-Javadoc)
@@ -33,17 +48,17 @@ public class NginxReader implements IRichSpout {
 	 */
 	@SuppressWarnings("rawtypes")
 	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
-		try {
-			String fileName = conf.get("wordsFile").toString();
-			
-			/*
-			 * LogSource logSource=dbServer.getLogsourceId(filename);
-			 * int logSourceId = logSource.getLogSourceId();
-			 */
-			this.fileReader = new FileReader(fileName);
-		} catch (Exception e) {
-			throw new RuntimeException("Errorreading file[" + conf.get("wordFile") + "]");
-		}
+//		try {
+//			String fileName = conf.get("wordsFile").toString();
+//			
+//			/*
+//			 * LogSource logSource=dbServer.getLogsourceId(filename);
+//			 * int logSourceId = logSource.getLogSourceId();
+//			 */
+//			this.fileReader = new FileReader(fileName);
+//		} catch (Exception e) {
+//			throw new RuntimeException("Errorreading file[" + conf.get("wordFile") + "]");
+//		}
 		this.collector = collector;
 	}
 
@@ -53,27 +68,69 @@ public class NginxReader implements IRichSpout {
 	}
 
 	public void nextTuple() {
-		// TODO Auto-generated method stub
-		String str;
-		BufferedReader reader = new BufferedReader(fileReader);
-		/*
-		 * hostname=header.get("****");
-		 * path=
-		 * filePattern=
-		 */
-		String hostname = "db50.photo.163.org";
-		String path = "/home/test";
-		String filePattern = "nginx.log";
-		try {
-			// read all lines
-			while ((str = reader.readLine()) != null) {
-				// emit the tuple(value = line)
-				logger.info("reader count:" + (++count));
-				collector.emit(new Values(hostname, path, filePattern, str));
+		
+		while (true) {
+			try {
+				logger.info("=====get a new channel======");
+				Channel channel = getChannel();
+				QueueingConsumer consumer = new QueueingConsumer(channel);
+				channel.basicConsume(queueName, true, consumer);
+				channel.basicQos(1);
+				while (true) {
+					QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+					String message = new String(delivery.getBody());
+
+					Map<String, Object> headers = delivery.getProperties().getHeaders();
+					String hostname = "";  
+				    String path =  "";  
+				    String filePattern =  "";
+					try{
+						hostname = headers.get("__DS_.fields.hostname").toString();  
+					    path =  headers.get("__DS_.fields._ds_target_dir").toString();  
+					    filePattern =  headers.get("__DS_.fields._ds_file_pattern").toString();  
+					    
+					    this.collector.emit(new Values(hostname, path, filePattern, message), message);
+						logger.debug("Consume: " + message);
+						logger.info("hostname: " + hostname + ", path: " + path + ", filePattern: " + filePattern);
+					}
+					catch(NullPointerException e){
+						logger.error("can't get header, hostname: " + hostname + ", path: " + path + ", file: " + filePattern, e);
+					}
+				}
 			}
-		} catch (Exception e) {
-			throw new RuntimeException("Errorreading tuple", e);
-		} 
+			catch (Exception e) {
+				logger.error("consume error, close connction", e);
+				if (channel != null) {
+					try {
+						channel.close();
+					}
+					catch (IOException e1) {
+						channel = null;
+					}
+				}
+			}
+		}
+		
+//		String str;
+//		BufferedReader reader = new BufferedReader(fileReader);
+//		/*
+//		 * hostname=header.get("****");
+//		 * path=
+//		 * filePattern=
+//		 */
+//		String hostname = "db50.photo.163.org";
+//		String path = "/home/test";
+//		String filePattern = "nginx.log";
+//		try {
+//			// read all lines
+//			while ((str = reader.readLine()) != null) {
+//				// emit the tuple(value = line)
+//				collector.emit(new Values(hostname, path, filePattern, str));
+//			}
+//		} catch (Exception e) {
+//			throw new RuntimeException("Errorreading tuple", e);
+//		} 
+		
 	}
 
 	public void ack(Object msgId) {
@@ -104,6 +161,43 @@ public class NginxReader implements IRichSpout {
 	public void deactivate() {
 		// TODO Auto-generated method stub
 
+	}
+	
+	private Connection getConnection() throws IOException {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(host);
+		factory.setPort(port);
+		connection = factory.newConnection();
+		return connection;
+	}
+	
+	private Channel getChannel() {
+		int count = 3;
+		while (count-- > 0) {
+			try {
+				if (connection == null) {
+					connection = getConnection();
+				}
+				return connection.createChannel();
+			}
+			catch (Exception e) {
+				logger.error("get channel error, try left: " + count, e); 
+				if (connection != null) {
+					try {
+						connection.close();
+					}
+					catch (Exception e1) {
+					}
+				}
+				connection = null;
+				try {
+					Thread.sleep(1000);
+				}
+				catch (InterruptedException e2) {
+				}
+			}
+		}
+		return null;
 	}
 
 }
--- watchlog-storm/src/main/java/com/netease/qa/log/storm/util/ConfigReader.java
+++ watchlog-storm/src/main/java/com/netease/qa/log/storm/util/ConfigReader.java
@@ -20,6 +20,21 @@ public class ConfigReader {
 	public static String MQ_HOST;
 	public static int MQ_PORT;
 	public static String MQ_QUEUE;
+	//storm的相关配置
+	public static int TOPOLOGY_WORKERS;
+	public static int TOPOLOGY_MAX_SPOUT_PENDING;
+	public static int TOPOLOGY_ACKER_EXECUTORS;
+	//异常日志topology的配置
+	public static int LOG_SPOUT;
+	public static int LOG_NORMALIZER_BOLT;
+	public static int LOG_FILTER_BOLT;
+	public static int LOG_ANALYSER_BOLT;
+	public static int RESULT_WRITER_BOLT;
+	//nginx日志topology的配置
+	public static int NGINX_READER_SPOUT;
+	public static int NGINX_NORMALIZER_BOLT;
+	public static int NGINX_FILTER_BOLT;
+	public static int NGINX_ANALYSER_BOLT;
 	
 	static{
 		Properties properties = new Properties();
@@ -41,6 +56,22 @@ public class ConfigReader {
 		MQ_HOST = properties.getProperty("mq.host");
 		MQ_PORT = Integer.valueOf(properties.getProperty("mq.port"));
 		MQ_QUEUE = properties.getProperty("mq.queue");
+		
+		TOPOLOGY_WORKERS = Integer.valueOf(properties.getProperty("topology_workers"));
+		TOPOLOGY_MAX_SPOUT_PENDING = Integer.valueOf(properties.getProperty("topology_max_spout_pending"));
+		TOPOLOGY_ACKER_EXECUTORS = Integer.valueOf(properties.getProperty("topology_acker_executors"));
+		
+		LOG_SPOUT = Integer.valueOf(properties.getProperty("mq_consumer.spout"));
+		LOG_NORMALIZER_BOLT = Integer.valueOf(properties.getProperty("log_normalizer.bolt"));
+		LOG_FILTER_BOLT = Integer.valueOf(properties.getProperty("log_filter.bolt"));
+		LOG_ANALYSER_BOLT = Integer.valueOf(properties.getProperty("log_analyser.bolt"));
+		RESULT_WRITER_BOLT = Integer.valueOf(properties.getProperty("result_writer.bolt"));
+		
+		NGINX_READER_SPOUT = Integer.valueOf(properties.getProperty("nginx_reader.spout"));
+		NGINX_NORMALIZER_BOLT = Integer.valueOf(properties.getProperty("nginx_normalizer.bolt"));
+		NGINX_FILTER_BOLT = Integer.valueOf(properties.getProperty("nginx_filter.bolt"));
+		NGINX_ANALYSER_BOLT = Integer.valueOf(properties.getProperty("nginx_analyze.bolt"));
+		
 	}
 	
 	
